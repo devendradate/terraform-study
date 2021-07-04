e272fde626958cd78ba773ad8d7670bea9ac08ac
Devendra <devendradate13@gmail.com>
2021-07-04 17:59:54 +0530

    Update ec2.tf

--- ec2-with-efs/ec2.tf
+++ ec2-with-efs/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "one" {
   ami           = var.ami
   instance_type = "t2.micro"
-  availability_zone = "<region>-1a"
+  availability_zone = "<region>-1a" # Create instance in 1a AZ
   key_name = "personal"
   security_groups = ["ec2-with-efs-sg"]
 
@@ -17,7 +17,7 @@ resource "aws_instance" "one" {
 resource "aws_instance" "two" {
   ami           = var.ami
   instance_type = "t2.micro"
-  availability_zone = "<region>-1b"
+  availability_zone = "<region>-1b" #create instance in 1b AZ
   key_name = "personal"
   security_groups = ["ec2-with-efs-sg"]
 
