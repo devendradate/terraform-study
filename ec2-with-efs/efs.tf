2914733c1381877a3e162cd353b50ef6f29e6dfd
Devendra <devendradate13@gmail.com>
2021-07-04 18:00:48 +0530

    Update efs.tf

--- ec2-with-efs/efs.tf
+++ ec2-with-efs/efs.tf
@@ -8,20 +8,20 @@ resource "aws_efs_file_system" "foo" {
 
 resource "aws_efs_mount_target" "alpha" {
   file_system_id = aws_efs_file_system.foo.id
-  subnet_id      = flatten(var.subnet_id)[0]
+  subnet_id      = flatten(var.subnet_id)[0] # fetch first subnet id from subnet vars
   security_groups = [aws_security_group.efs.id]
 
 }
 
 resource "aws_efs_mount_target" "alpha1" {
   file_system_id = aws_efs_file_system.foo.id
-  subnet_id      = flatten(var.subnet_id)[1]
+  subnet_id      = flatten(var.subnet_id)[1] # fetch second subnet id from subnet vars
   security_groups = [aws_security_group.efs.id]
 }
 
 resource "aws_efs_mount_target" "alpha2" {
   file_system_id = aws_efs_file_system.foo.id
-  subnet_id      = flatten(var.subnet_id)[2]
+  subnet_id      = flatten(var.subnet_id)[2] # fetch third subnet id from subnet vars
   security_groups = [aws_security_group.efs.id]
 
 }
