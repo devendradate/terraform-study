ed29b5b199901bb389831d72b88aaa1d8298fd8f
Devendra <devendradate13@gmail.com>
2021-11-30 21:30:28 +0530

    Create iam.tf

---
+++ sqs/iam.tf
@@ -0,0 +1,38 @@
+data "aws_iam_policy_document" "consumer" {
+  statement {
+    effect    = "Allow"
+    resources = [aws_sqs_queue.terraform_queue.arn]
+
+    actions = [
+      "sqs:GetQueueAttributes",
+      "sqs:GetQueueUrl",
+      "sqs:ReceiveMessage",
+      "sqs:DeleteMessage*",
+      "sqs:PurgeQueue",
+      "sqs:ChangeMessageVisibility*"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "pusher" {
+  statement {
+    effect    = "Allow"
+    resources = [aws_sqs_queue.terraform_queue.arn]
+
+    actions = [
+      "sqs:GetQueueAttributes",
+      "sqs:GetQueueUrl",
+      "sqs:SendMessage*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "consumer" {
+  name   = "sqs-${var.sqs_name}-consumer"
+  policy = data.aws_iam_policy_document.consumer.json
+}
+
+resource "aws_iam_policy" "pusher" {
+  name   = "sqs-${var.sqs_name}-pusher"
+  policy = data.aws_iam_policy_document.pusher.json
+}%
