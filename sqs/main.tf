b7c42f9fa0eec234beb13d4f6db20fff68355f93
Devendra <devendradate13@gmail.com>
2021-11-30 21:31:27 +0530

    Create main.tf

---
+++ sqs/main.tf
@@ -0,0 +1,8 @@
+resource "aws_sqs_queue" "terraform_queue" {
+  name                      = var.sqs_name
+  visibility_timeout_seconds = var.visibility_timeout_seconds
+  delay_seconds             = var.delay_seconds
+  max_message_size          = var.max_message_size
+  message_retention_seconds = var.message_retention_seconds
+  receive_wait_time_seconds = var.receive_wait_time_seconds
+}
