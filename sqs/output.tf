2947c58d1bf90b44cdbcb9778df7fb6b9e396bf5
Devendra <devendradate13@gmail.com>
2021-11-30 21:31:49 +0530

    Create output.tf

---
+++ sqs/output.tf
@@ -0,0 +1,19 @@
+output "arn" {
+  value       = aws_sqs_queue.terraform_queue.arn
+  description = "The ARN for the created Amazon SQS queue"
+}
+
+output "id" {
+  value       = aws_sqs_queue.terraform_queue.id
+  description = "The URL for the created Amazon SQS queue"
+}
+
+output "pusher_policy_arn" {
+  value       = aws_iam_policy.pusher.arn
+  description = "The ARN of the IAM policy used by the queue consumer / worker"
+}
+
+output "consumer_policy_arn" {
+  value       = aws_iam_policy.consumer.arn
+  description = "The ARN of the IAM policy used by the queue pusher"
+}
